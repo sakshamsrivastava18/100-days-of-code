2a6a53e51100df985da96e3294574b826daa8a32
Saksham Srivastava <sakshamsrivastava05112008@gmail.com>
2026-08-29 11:16:50 +0530

    Add files via upload

---
+++ day3b.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a, b, c;
+    printf("enter two numbers:");
+    scanf("%d %d", &a, &b);
+    c = a;
+    a = b;
+    b = c;
+    printf("after swapping: %d %d", a, b);
+    return 0;
+}
---
+++ day4a.c
@@ -0,0 +1,12 @@
+//Write a program to swap two numbers using a third variable.
+#include<stdio.h>
+int main(){
+    int a, b, c;
+    printf("enter two numbers:");
+    scanf("%d %d", &a, &b);
+    c = a;
+    a = b;
+    b = c;
+    printf("after swapping: %d %d", a, b);
+    return 0;
+}
