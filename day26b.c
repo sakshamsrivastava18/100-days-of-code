248b39b017087237cb6634ee39b5b31823378a37
Saksham Srivastava <sakshamsrivastava05112008@gmail.com>
2026-09-04 15:24:08 +0530

    Add files via upload

---
+++ day26b.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Print 1 star
+    for (i = 1; i <= 1; i++) {
+        printf("*\n");
+    }
+
+    printf("\n");
+
+    // Print 3 stars
+    for (i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+
+    printf("\n");
+
+    
+    for (i = 1; i <= 5; i++) {
+        printf("*\n");
+    }
+
+    printf("\n");
+
+    
+    for (i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+
+    return 0;
+}
