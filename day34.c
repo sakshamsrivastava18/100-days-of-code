98f3a06bc0d32f3d808809bb46ce28257528ea66
Saksham Srivastava <sakshamsrivastava05112008@gmail.com>
2026-09-12 19:02:26 +0530

    Add files via upload

---
+++ day34.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, pos, element;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the position to insert: ");
+    scanf("%d", &pos);
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &element);
+
+    // Shift elements one position to the right
+    for (i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert the new element
+    arr[pos - 1] = element;
+
+    n++;
+
+    printf("Array after insertion:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
---
+++ day34b.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, pos;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the position to delete: ");
+    scanf("%d", &pos);
+
+    // Shift elements one position to the left
+    for (i = pos - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    n--;
+
+    printf("Array after deletion:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
