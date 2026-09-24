d8474a8f369085c9052c6a1628c2845786fc807d
Saksham Srivastava <sakshamsrivastava05112008@gmail.com>
2026-09-24 22:50:34 +0530

    Add files via upload

---
+++ day46b.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count[26] = {0};
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            count[str[i] - 'a']++;
+
+            if (count[str[i] - 'a'] == 2) {
+                printf("First repeating lowercase alphabet: %c\n", str[i]);
+                return 0;
+            }
+        }
+    }
+
+    printf("No repeating lowercase alphabet found.\n");
+
+    return 0;
+}
